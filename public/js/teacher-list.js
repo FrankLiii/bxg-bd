7b2f3e27378fb875d4e3bab3e1291165ebc7aa9d
lisi <lisi@itcast.com>
2017-09-21 20:39:49 +0800

    完成启用和注销功能

--- public/js/teacher-list.js
+++ public/js/teacher-list.js
@@ -7,6 +7,35 @@ define(['jquery','template'],function($,template){
 			// 解析数据 渲染页面
 			var html=template('teacherTpl',{list:data.result});
 			$('#teacherInfo').html(html);
+
+			// 启用注销功能
+			$(".eod").click(function(){
+				var that=this;
+				var td=$(this).closest("td");
+				var tcId=td.attr("data-tcId");
+				var status=td.attr("data-status");
+				$.ajax({
+					type:"post",
+					url:"/api/teacher/handle",
+					dataType:'json',
+					data:{tc_id:tcId,tc_status:status},
+					success:function(data){
+						if(data.code==200){
+							td.attr('data-status',data.result.tc_status);
+
+							if(data.result.tc_status==0){
+								$(that).text("注销");
+							}else{
+								$(that).text("启用");
+							}
+						}	
+					}
+
+				})
+
+			})
+
 		}
 	})
+	
 })
