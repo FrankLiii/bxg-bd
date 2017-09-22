825e6459238daffab7db20524595801735f2bd5f
lisi <lisi@itcast.com>
2017-09-22 11:47:36 +0800

    完成讲师添加和编辑功能

--- public/js/teacher-add.js
+++ public/js/teacher-add.js
@@ -11,14 +11,33 @@ define(['jquery','template','until'],function($,template,until){
 				data.result.opera="编辑讲师";
 				var html=template('teacherTpl',data.result);
 				$("#teacherInfo").html(html);
-
+				submitForm("/api/teacher/update");
 			}
 		})
 	}else{
 		// 添加功能
 		var html=template('teacherTpl',{opera:'添加讲师'});
 		$("#teacherInfo").html(html);
+		submitForm('/api/teacher/add');
+	}
 
+	// 实现表单提交
+	function submitForm(url){
+		$("#teacherBtn").click(function(){
+			$.ajax({
+				type:"post",
+				url:url,
+				data:$("#teacherForm").serialize(),
+				dataType:'json',
+				success:function(data){
+					if(data.code==200){
+						location.href="/teacher/list";
+					}
+				}
+			})
+			
+		})
+		
 	}
 	
 })
