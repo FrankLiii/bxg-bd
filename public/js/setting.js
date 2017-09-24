7725fcd51616fc38fbf67ba5e8d6e8ffa031674d
lisi <lisi@itcast.com>
2017-09-24 22:32:15 +0800

    完成头像上传功能

--- public/js/setting.js
+++ public/js/setting.js
@@ -1,4 +1,5 @@
-define(['jquery','template'],function($,template){
+define(['jquery','template','uploadify'],function($,template){
+	// 获取数据，渲染页面
 	$.ajax({
 		type:"get",
 		url:"/api/teacher/profile",
@@ -6,6 +7,22 @@ define(['jquery','template'],function($,template){
 		success:function(data){
 			var html=template("settingsTpl",data.result);
 			$("#settingsInfo").html(html);
+			// 上传图片
+			$('#upfile').uploadify({
+				width:120,
+				height:120,
+				buttonText:'',
+				itemTemplate:"<span></span>",
+			 	swf:'/public/assets/uploadify/uploadify.swf',
+			 	uploader:'/api/uploader/avatar',
+			 	fileObjName:'tc_avatar',
+			 	onUploadSuccess:function(a,b){
+			 		var obj=JSON.parse(b);
+			 		$(".preciew img").attr('src',obj.result.path);
+			 		
+			 	}
+			});
+			// 处理省市县三级联动
 		}
 	})
 })
