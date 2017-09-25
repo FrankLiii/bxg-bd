ba128f4f17577466ec52c4a4cdff9e24479e4225
lisi <lisi@itcast.com>
2017-09-25 22:41:46 +0800

    完成课程列表展示

--- public/js/course-list.js
+++ public/js/course-list.js
@@ -1,4 +1,13 @@
 define(['jquery','template','until'],function($,template,until){
 	// 设置侧边菜单选中
 	until.setMenu(location.pathname)
+	$.ajax({
+		type:'get',
+		url:'/api/course',
+		dataType:'json',
+		success:function(data){
+			var html=template('courseTpl',{list:data.result});
+			$('#courseInfo').html(html);
+		}
+	})
 })
