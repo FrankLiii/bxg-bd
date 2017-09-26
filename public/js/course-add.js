798773d0196e1bac6ca060194c7f5f51fb5ccfa7
lisi <lisi@itcast.com>
2017-09-26 19:01:33 +0800

    完成二级分类联动操作

--- public/js/course-add.js
+++ public/js/course-add.js
@@ -3,14 +3,16 @@ define(['jquery','template','until','form'],function($,template,until){
   until.setMenu(location.pathname);
   // 绑定表单提交单击事件
   $('#courseBtn').click(function(){
+
       $('#courseForm').ajaxSubmit({
         type:'post',
         url:'/api/course/create',
         dataType:'json',
         success:function(data){
+          
           if(data.code==200){
             location.href='/course/basic?cs_id='+data.result.cs_id;
-           
+
           }
         }
       })
--- public/js/course-basic.js
+++ public/js/course-basic.js
@@ -1,4 +1,4 @@
-define(['jquery','template','until'],function($,template,until){
+define(['jquery','template','until','ckeditor'],function($,template,until,CKEDITOR){
   // 设置导航菜单选中
   until.setMenu('/course/add');
   // 获取课程ID
@@ -16,9 +16,34 @@ define(['jquery','template','until'],function($,template,until){
       }else{
           data.result.opera="课程添加";
       }
+      console.log(data);
       var html=template('basicTpl',data.result);
       $('#basicInfo').html(html);
-     
+      // 处理二级分类的下拉联动
+      $("#firstType").change(function(){
+        // 获取一级分类的cg_id;
+          var pid=$(this).val();
+         // 根据一级分类查询二级分类数据
+         $.ajax({
+          type:'get',
+          url:'/api/category/child',
+          data:{cg_id:pid},
+          dataType:'json',
+          success:function(data){
+            // 
+            var tpl='<option value="">请选择二级分类</option>{{each list}}<option value="{{$value.cg_id}}">{{$value.cg_name}}</option>{{/each}}'
+            var html=template.render(tpl,{list:data.result}) ;
+            $("#secondType").html(html);
+          }
+         })
+      });
+      // 处理富文本
+      CKEDITOR.replace('editor',{
+        toolbarGroups : [
+              { name: 'clipboard', groups: [ 'clipboard', 'undo' ] },
+              { name: 'editing', groups: [ 'find', 'selection', 'spellchecker', 'editing' ] }
+            ]
+          });
     }
   })
 
